2e125791fd2b397515c337451a86af510c31335c
mo-adel-tronic <mo.adel.developer@gmail.com>
2024-06-27 23:02:44 +0300

    handle structure and routes

---
+++ lib/config/routes/app_routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:pizza/core/constants/routes_name.dart';
+
+class AppRouter {
+  Route? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case AppCharRoute.splash:
+        
+    }
+    return null;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:pizza/config/routes/app_routes.dart';
+
+void main() {
+  runApp(DevicePreview(builder: (context) => MyApp(
+    appRouter: AppRouter(),
+  )));
+}
+
+class MyApp extends StatelessWidget {
+  final AppRouter appRouter;
+  const MyApp({
+    super.key,
+    required this.appRouter,
+  });
+  @override
+  Widget build (BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      builder: DevicePreview.appBuilder,
+      onGenerateRoute: appRouter.generateRoute,
+    );
+  }
+}
